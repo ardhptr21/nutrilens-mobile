3cde259041cec17410b67ada7c03e34493f2c965
farelrafli <152679404+farelrafli@users.noreply.github.com>
2025-12-01 10:59:36 +0700

    Update history_page.dart

--- lib/presentation/pages/history_page.dart
+++ lib/presentation/pages/history_page.dart
@@ -1,126 +1,63 @@
+// lib/presentation/pages/history_page.dart
 import 'package:flutter/material.dart';
 import 'package:nutrilens/models/nutrition_model.dart';
+import 'package:nutrilens/presentation/pages/history_detail_page.dart';
 import 'package:nutrilens/presentation/widgets/card/history_card_widget.dart';
 
-import 'history_detail_page.dart';
+class HistoryPage extends StatelessWidget {
+  final List<NutritionModel> history;
 
-class HistoryPage extends StatefulWidget {
-  const HistoryPage({super.key});
-
-  @override
-  State<HistoryPage> createState() => _HistoryPageState();
-}
-
-class _HistoryPageState extends State<HistoryPage> {
-  late final List<NutritionModel> _items;
-
-  @override
-  void initState() {
-    super.initState();
-    _generateDummyData();
-  }
-
-  void _generateDummyData() {
-    _items = [
-      NutritionModel(
-        id: 'history-0',
-        date: DateTime(2024, 12, 28),
-        totalCalories: 2150,
-        calorieGoal: 2000,
-        protein: 85,
-        fat: 65,
-        carbs: 250,
-      ),
-      NutritionModel(
-        id: 'history-1',
-        date: DateTime(2024, 12, 27),
-        totalCalories: 1950,
-        calorieGoal: 2000,
-        protein: 78,
-        fat: 58,
-        carbs: 238,
-      ),
-      NutritionModel(
-        id: 'history-2',
-        date: DateTime(2024, 12, 26),
-        totalCalories: 2300,
-        calorieGoal: 2000,
-        protein: 92,
-        fat: 72,
-        carbs: 275,
-      ),
-      NutritionModel(
-        id: 'history-3',
-        date: DateTime(2024, 12, 25),
-        totalCalories: 1850,
-        calorieGoal: 2000,
-        protein: 72,
-        fat: 52,
-        carbs: 220,
-      ),
-      NutritionModel(
-        id: 'history-4',
-        date: DateTime(2024, 12, 24),
-        totalCalories: 2050,
-        calorieGoal: 2000,
-        protein: 81,
-        fat: 62,
-        carbs: 245,
-      ),
-      NutritionModel(
-        id: 'history-5',
-        date: DateTime(2024, 12, 23),
-        totalCalories: 1900,
-        calorieGoal: 2000,
-        protein: 75,
-        fat: 55,
-        carbs: 230,
-      ),
-    ];
-  }
+  const HistoryPage({super.key, this.history = const []});
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
+    final defaultHistory = [
+      NutritionModel(
+        id: 'h-1',
+        date: DateTime(2025, 12, 1),
+        totalCalories: 1280,
+        calorieGoal: 2000,
+        protein: 50,
+        carbs: 80,
+        fat: 13,
+      ),
+      NutritionModel(
+        id: 'h-2',
+        date: DateTime(2025, 11, 30),
+        totalCalories: 1500,
+        calorieGoal: 2000,
+        protein: 70,
+        carbs: 120,
+        fat: 20,
+      ),
+      NutritionModel(
+        id: 'h-3',
+        date: DateTime(2025, 11, 29),
+        totalCalories: 2300,
+        calorieGoal: 2000,
+        protein: 90,
+        carbs: 200,
+        fat: 40,
+      ),
+    ];
 
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 40.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              child: Text(
-                'Riwayat Nutrisi',
-                style: theme.textTheme.headlineSmall?.copyWith(
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-            ),
-            Expanded(
-              child: ListView.separated(
-                itemCount: _items.length,
-                separatorBuilder: (context, _) => const SizedBox(height: 12),
-                itemBuilder: (context, index) {
-                  final item = _items[index];
-                  return HistoryCardWidget(
-                    item: item,
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              HistoryDetailPage(nutrition: item),
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
+    final items = history.isNotEmpty ? history : defaultHistory;
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Riwayat Nutrisi')),
+      body: ListView.separated(
+        padding: const EdgeInsets.all(20),
+        itemCount: items.length,
+        separatorBuilder: (_, __) => const SizedBox(height: 12),
+        itemBuilder: (context, index) {
+          final item = items[index];
+          return HistoryCardWidget(
+            item: item,
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (_) => HistoryDetailPage(item: item)));
+            },
+          );
+        },
       ),
     );
   }
